02f76a23267d321d88d8be6e2f18dfe87af6a8e0
Eksiart <eksiart-2@mail.ru>
2022-10-06 16:45:33 +0800

    add EntityAdapter

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -1,14 +1,16 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { activeFilterChanged, fetchFilters } from './filtersSlice';
+import { activeFilterChanged, fetchFilters, selectAll } from './filtersSlice';
+import store from '../../store'
 
 import Spinner from '../spinner/Spinner';
 import classNames from 'classnames';
 
 const HeroesFilters = () => {
     
-    const {filters, filtersLoadingStatus, activeFilter} = useSelector(state => state.filters);
+    const {filtersLoadingStatus, activeFilter} = useSelector(state => state.filters);
+    const filters = selectAll(store.getState());
     const dispatch = useDispatch();
 
     useEffect(() => {
