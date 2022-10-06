cd74498b774f738edf996c158d587f51bc60dfb2
Eksiart <eksiart-2@mail.ru>
2022-10-06 15:04:43 +0800

    add createAsyncThunk

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -1,9 +1,7 @@
-import {useHttp} from '../../hooks/http.hook';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { fetchFilters } from '../../actions';
-import { activeFilterChanged } from '../heroesFilters/filtersSlice';
+import { activeFilterChanged, fetchFilters } from './filtersSlice';
 
 import Spinner from '../spinner/Spinner';
 import classNames from 'classnames';
@@ -12,10 +10,9 @@ const HeroesFilters = () => {
     
     const {filters, filtersLoadingStatus, activeFilter} = useSelector(state => state.filters);
     const dispatch = useDispatch();
-    const {request} = useHttp();
 
     useEffect(() => {
-        dispatch(fetchFilters(request));
+        dispatch(fetchFilters());
          // eslint-disable-next-line
     }, [])
 
